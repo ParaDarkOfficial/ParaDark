5f374acad3561fab69a240f1d734afa94b3e01a2
prathoseraaj <prathoseraaj0312@gmail.com>
2025-10-30 00:36:48 +0530

    fix: comment

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -5,16 +5,9 @@ import Footer from "@/components/Footer";
 const page = () => {
   return (
     <div className="relative min-h-screen">
-      {/* Background */}
       <div className="fixed inset-0 -z-10 h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]" />
-      
-      {/* Navbar */}
       <Navbar />
-      
-      {/* Hero Section */}
       <Hero />
-      
-      {/* Footer */}
       <Footer />
     </div>
   );
